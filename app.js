edec9960d8451e455ee5140074fe48bb3ab18805
JoannaMaaae <joannamaeconcepcion@gmail.com>
2019-05-09 09:02:58 +0800

    added project files

---
+++ app.js
@@ -0,0 +1,58 @@
+new Vue ({
+    el: '#joanna',
+    data: {
+        name: 'Joanna',
+        age: 19,
+        name1: '',
+        age1: '',
+        job: 'Manager',
+        x: 0,
+        y: 0,
+        a: 0,
+        b: 0,
+        website: 'http://www.spotty.com',
+        websiteTag: '<a href="http://www.milo.com"> Milo </a>'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good' + time + ' ' + this.name;
+        }, 
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age-= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function(){
+            alert('Why did  you clicked me?');
+        },
+        logName: function(){
+            console.log('You entered your name.');
+        },
+        logAge: function(){
+            console.log('You entered your age.');
+        }
+        /* addToA: function(){
+            console.log('Age is added in A.');
+            return this.a + this.age;
+        },
+        addToB: function(){
+            console.log('Age is added in B.');
+            return this.b + this.age;    
+        }*/
+    },
+    computed: {
+        addToA: function(){
+            console.log('Age is added in A.');
+            return this.a + this.age;
+        },
+        addToB: function(){
+            console.log('Age is added in B.');
+            return this.b + this.age;    
+        }
+    }
+});
